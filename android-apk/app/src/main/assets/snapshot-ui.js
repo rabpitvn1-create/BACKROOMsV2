b562a8bc6386acdc62818bc50ed132e926013e67
rabpitvn1-create <rabpitvn1@gmail.com>
2026-09-21 00:10:19 +0700

    Fix Cao Minh Explorer overlay metadata hash

--- android-apk/app/src/main/assets/snapshot-ui.js
+++ android-apk/app/src/main/assets/snapshot-ui.js
@@ -6,7 +6,7 @@ var SnapshotOverlayLayout = (function(){
   // BEGIN GENERATED OVERLAY METRICS
   var bundledMetrics={
     "cao_minh_entity_overlay.png":{"width":1122,"height":1402,"paint":{"left":1,"top":0,"right":1122,"bottom":1386},"body":{"left":2,"top":3,"right":1122,"bottom":1376},"sha256":"980aaaf8a8575d41ab66b95d8ab24ca7052b66677a1a15e0eb6433c8b716eb47"},
-    "cao_minh_snapshot_overlay.png":{"width":1122,"height":1402,"paint":{"left":54,"top":16,"right":1020,"bottom":1389},"body":{"left":54,"top":17,"right":1017,"bottom":1385},"sha256":"e02f27c3125c3c4c3c3eb4b4314c93bab89d0ac18c92f8038ee2533d616bbd6"},
+    "cao_minh_snapshot_overlay.png":{"width":1122,"height":1402,"paint":{"left":54,"top":16,"right":1020,"bottom":1389},"body":{"left":54,"top":17,"right":1017,"bottom":1385},"sha256":"e02f27c3125c3c4c3c3eb4b4314c93bab89d0ac18c92f8038ee2533d616bbd6e"},
     "entity/biological_pipeline.png":{"width":1086,"height":1448,"paint":{"left":112,"top":14,"right":973,"bottom":1434},"body":{"left":113,"top":14,"right":972,"bottom":1433},"sha256":"0474fa10a7ff4f06089df8c5b937576278a11951ae73f24cd2c1afd5cb78f413"},
     "entity/cable_mimic.png":{"width":1254,"height":1254,"paint":{"left":339,"top":19,"right":963,"bottom":1237},"body":{"left":340,"top":21,"right":961,"bottom":1237},"sha256":"15ccdd6a14c0e80e0acec072090aaeb134c6c6b66570cc4e05c9041017399fdb"},
     "entity/clump.png":{"width":1254,"height":1254,"paint":{"left":1,"top":10,"right":1254,"bottom":1227},"body":{"left":3,"top":10,"right":1250,"bottom":1226},"sha256":"a7776f353412753875d4742498135ea7053f60512a82fdf48c350c1e03135b21"},
